5feb817ea08470578822b996132a46cb4b1814fc
Vien Dinh <maxvien@gmail.com>
2019-07-06 07:25:26 +0700

    Render multiple lines of plain text.

--- src/components/Content/PlainTextViewer.js
+++ src/components/Content/PlainTextViewer.js
@@ -1,3 +1,7 @@
+import React from 'react';
+
 export default function PlainTextViewer({ file }) {
-  return file.textContent;
+  return file.textContent.split('\n').map((i, key) => {
+    return <p key={key}>{i}</p>;
+  });
 }
